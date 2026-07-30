3c6c5dccabc794dba26f0eaaa6cb99ab97788d81
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-30 18:41:09 +0530

    Create problem.java

--- Problem.java
+++ Problem.java
@@ -1,1 +1,22 @@
+public class Problem {
 
+    int problemId;
+    String problemName;
+    String difficulty;
+    String platform;
+
+    public Problem(int problemId, String problemName, String difficulty, String platform) {
+        this.problemId = problemId;
+        this.problemName = problemName;
+        this.difficulty = difficulty;
+        this.platform = platform;
+    }
+
+    public void displayProblem() {
+        System.out.println("----------------------------");
+        System.out.println("Problem ID : " + problemId);
+        System.out.println("Problem Name : " + problemName);
+        System.out.println("Difficulty : " + difficulty);
+        System.out.println("Platform : " + platform);
+    }
+}
